835fc86dded885a41cd6ec3f2be532650409fc0c
AndrewArkless <andrewarkless@blueyonder.co.uk>
2017-04-17 17:53:22 +0100

    Initial COmmit

--- src/test/scala/BowlingSpec.scala
+++ src/test/scala/BowlingSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.{FlatSpec, Matchers, WordSpecLike, _}
 class BowlingSpec extends WordSpecLike with Matchers {
 
    "Calling BowlingScore"  should {
+     //Basic tests
      "Return 0 when player has not started"  in {
       val emptyFrame=List[Frame]()
       Bowling.score(emptyFrame) shouldBe 0
@@ -19,6 +20,27 @@ class BowlingSpec extends WordSpecLike with Matchers {
       Bowling.score(fiveFrame) shouldBe 5
     }
 
+     "Return a 50 when user rolls Frames which add to 50"   in {
+       val testFrame=List[Frame](
+         Frame( 1 , 4 ),Frame( 1 , 4 ),
+         Frame( 1 , 4 ),Frame( 1 , 4 ),
+         Frame( 1 , 4 ),Frame( 1 , 4 ),
+         Frame( 1 , 4 ),Frame( 1 , 4 ),
+         Frame( 1 , 4 ),Frame( 1 , 4 ))
+       Bowling.score(testFrame) shouldBe 50
+     }
+
+     "Return a 0 when user rolls Frames which add to 0"   in {
+       val testFrame=List[Frame](
+         Frame( 0 , 0 ),Frame( 0 , 0 ),
+         Frame( 0 , 0 ),Frame( 0 , 0 ),
+         Frame( 0 , 0 ),Frame( 0 , 0 ),
+         Frame( 0 , 0 ),Frame( 0 , 0 ),
+         Frame( 0 , 0 ),Frame( 0 , 0 ))
+       Bowling.score(testFrame) shouldBe 0
+     }
+
+     //Strikes
      "Calculate a strike correctly"  in {
       val strikeFrame=List[Frame](Frame( 10 , 0 ),Frame( 5 , 2 ))
       Bowling.score(strikeFrame) shouldBe 24
@@ -28,44 +50,71 @@ class BowlingSpec extends WordSpecLike with Matchers {
       Bowling.score(strikeFrame) shouldBe 47
     }
 
-
-     "Calculate All strikes in a row correctly except last"  in {
-       val AllStrikesFrame=List[Frame](Frame( 10 , 0 ),Frame( 10 , 0 ),
-         Frame( 10 , 0 ),Frame( 3 , 4 ))
-//         Frame( 10 , 0 ),Frame( 10 , 0 ),
-//         Frame( 10 , 0 ),Frame( 3,  4 ))
-//         Frame( 10 , 0 ),Frame( 2 , 3 ))
-//         Frame( 10 , 0 ),Frame( 10 , 0 ))
-       Bowling.score(AllStrikesFrame) shouldBe 77
-
-     }
      "Calculate All strikes in a row correctly"  in {
-      val AllStrikesFrame=List[Frame](Frame( 10 , 0 ),Frame( 10 , 0 ),
-        Frame( 10 , 0 ),Frame( 10 , 0 ),
-        Frame( 10 , 0 ),Frame( 10 , 0 ),
-        Frame( 10 , 0 ),Frame( 10 , 0 ),
-        Frame( 10 , 0 ),Frame( 10 , 0 ),
-        Frame( 10 , 0 ),Frame( 10 , 0 ))
-      Bowling.score(AllStrikesFrame) shouldBe 300
+       val AllStrikesFrame=List[Frame](
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ))
+       Bowling.score(AllStrikesFrame) shouldBe 300
+     }
 
-    }
+     "Calculate 9 strikes in a row correctly except last which is a open no bonus"  in {
+       val AllStrikesFrame=List[Frame](
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10, 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 3,  4 )
+       )
+       Bowling.score(AllStrikesFrame) shouldBe 257
+     }
+     "Calculate 10 strikes in a row except bonus which is open correctly "  in {
+       val AllStrikesFrame = List[Frame](
+         Frame(10, 0), Frame(10, 0),
+         Frame(10, 0), Frame(10, 0),
+         Frame(10, 0), Frame(10, 0),
+         Frame(10, 0), Frame(10, 0),
+         Frame(10, 0), Frame(10, 0),
+         Frame(2, 5))
+     Bowling.score(AllStrikesFrame) shouldBe 279
+   }
+
+     "Calculate All strikes in a row except bonus which is a Spare correctly"  in {
+       val AllStrikesFrame = List[Frame](
+         Frame(10, 0), Frame(10, 0),
+         Frame(10, 0), Frame(10, 0),
+         Frame(10, 0), Frame(10, 0),
+         Frame(10, 0), Frame(10, 0),
+         Frame(10, 0), Frame(10, 0),
+         Frame(9 ,1))
+       Bowling.score(AllStrikesFrame) shouldBe 289
+     }
+
+
+     //Spares
      "Calculate a spare correctly"  in {
       val spareFrame=List[Frame](Frame( 1 , 9 ),Frame( 2 , 6 ))
       Bowling.score(spareFrame) shouldBe 20
     }
 
      "Calculate 2 spares in a row"  in {
-      val spareFrame=List[Frame](Frame( 1 , 9 ),Frame( 5 , 5 ),Frame( 2 , 3 ))
+      val spareFrame=List[Frame](
+        Frame( 1 , 9 ),
+        Frame( 5 , 5 ),
+        Frame( 2 , 3 ))
       Bowling.score(spareFrame) shouldBe 32
     }
 
      "Calculate all spares in a row except last"  in {
-      val spareFrame=List[Frame](Frame( 5 , 5 ),Frame( 5 , 5 ),
+      val spareFrame=List[Frame](
         Frame( 5 , 5 ),Frame( 5 , 5 ),
         Frame( 5 , 5 ),Frame( 5 , 5 ),
         Frame( 5 , 5 ),Frame( 5 , 5 ),
-        Frame( 5 , 5 ),
-        Frame( 5 , 0 ))
+        Frame( 5 , 5 ),Frame( 5 , 5 ),
+        Frame( 5 , 5 ),Frame( 5 , 0 ))
       Bowling.score(spareFrame) shouldBe 140
     }
 
@@ -81,45 +130,93 @@ class BowlingSpec extends WordSpecLike with Matchers {
         Bowling.score(spareFrame) shouldBe 154
     }
 
-     "Return a 50 when user rolls Frames which add to 50"   in {
-        val hundredFrame=List[Frame](Frame( 1 , 4 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-                                    Frame( 1 , 4 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-                                    Frame( 1 , 4 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-                                    Frame( 1 , 4 ))
-        Bowling.score(hundredFrame) shouldBe 50
-    }
+     //Mixtures
+
+     "Calculate a mixture of strikes and spares ending on a open in a row correctly"  in {
+       val AllStrikesFrame=List[Frame](
+         Frame( 10 , 0 ),Frame( 9 , 1 ),
+         Frame( 10 , 0 ),Frame( 9 , 1 ),
+         Frame( 10 , 0 ),Frame( 9, 1 ),
+         Frame( 10 , 0 ),Frame( 9 , 1 ),
+         Frame( 10 , 0 ),Frame( 9, 0 )
+       )
+       Bowling.score(AllStrikesFrame) shouldBe 188
+     }
+
+     "Calculate a random mixture of strikes and spares ending on a open in a row correctly"  in {
+       val AllStrikesFrame=List[Frame](
+         Frame( 10 , 0 ),Frame( 9 , 1 ),
+         Frame( 10 , 0 ),Frame( 9 , 1 ),
+         Frame( 10 , 0 ),Frame( 9, 1 ),
+         Frame( 10 , 0 ),Frame( 9 , 1 ),
+         Frame( 10 , 0 ),Frame( 10, 0 ),
+         Frame( 2,  6)
+       )
+       Bowling.score(AllStrikesFrame) shouldBe 200
+     }
+
+
+     "Calculate 10 strikes in a row correctly 1 strike bonus and 1 Spare "  in {
+       val AllStrikesFrame=List[Frame](
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 10 , 0 ),
+         Frame( 10 , 0 ),Frame( 9 , 0 ))
+       Bowling.score(AllStrikesFrame) shouldBe 299
+     }
+
 
      "Return a 56 when user a spare and then standard Frames"   in {
-      val hundredFrame=List[Frame](Frame( 9 , 1 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ))
-      Bowling.score(hundredFrame) shouldBe 56
+      val testFrame=List[Frame](
+        Frame( 9 , 1 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ),Frame( 1 , 4 ))
+      Bowling.score(testFrame) shouldBe 56
     }
 
      "Return a 60 when user a has a strike and then standard Frames"   in {
-      val hundredFrame=List[Frame](Frame( 10 , 0 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ))
-      Bowling.score(hundredFrame) shouldBe 60
+      val testFrame=List[Frame](
+        Frame( 10 , 0 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ), Frame( 1 , 4 ),
+        Frame( 1 , 4 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ), Frame( 1 , 4 ))
+      Bowling.score(testFrame) shouldBe 60
     }
 
      "Return a 66 when user a has a strike and a spare and then standard Frames"   in {
-      val hundredFrame=List[Frame](Frame( 10 , 0 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ),Frame( 5 , 5 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ))
-      Bowling.score(hundredFrame) shouldBe 66
+      val testFrame=List[Frame](
+        Frame( 10 , 0 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ), Frame( 1 , 4 ),
+        Frame( 5 , 5 ), Frame( 1 , 4 ),
+        Frame( 1 , 4 ), Frame( 1 , 4 ),
+        Frame( 1 , 4 ), Frame( 1 , 4 ))
+      Bowling.score(testFrame) shouldBe 66
+    }
+     "Return a 60 when user a has a 2 strikes and 2 spares and then standard Frames"   in {
+      val testFrame=List[Frame](
+        Frame( 10 , 0 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ), Frame( 1 , 4 ),
+        Frame( 5 , 5 ),Frame( 1 , 4 ),
+        Frame( 1 , 4 ),Frame( 10 , 0 ),
+        Frame( 9 , 1 ),Frame( 1 , 4 ))
+      Bowling.score(testFrame) shouldBe 87
     }
 
-     "Return a 60 when user a has a 2 strikes and 2 spares and then standard Frames"   in {
-      val hundredFrame=List[Frame](Frame( 10 , 0 ),Frame( 1 , 4 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ),Frame( 5 , 5 ),Frame( 1 , 4 ),
-        Frame( 1 , 4 ),Frame( 10 , 0 ),Frame( 9 , 1 ),
-        Frame( 1 , 4 ))
-      Bowling.score(hundredFrame) shouldBe 87
-    }
+     "Random test from original requirement calulate correctly" in {
+       val testFrame=List[Frame](
+         Frame( 1 , 4 ),Frame( 4 , 5 ),
+         Frame( 6 , 4 ), Frame( 5 , 5 ),
+         Frame( 10 , 0 ),Frame( 0 , 1 ),
+         Frame( 7 , 3 ),Frame( 6 , 4 ),
+         Frame( 10 , 0 ),Frame( 2 , 8),
+         Frame(6,0))
+       Bowling.score(testFrame) shouldBe 133
+     }
   }
 
 }
